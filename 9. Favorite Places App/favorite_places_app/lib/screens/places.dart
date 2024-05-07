eb4ceee34c47fe2c9c1f64715aabb775048f786d
Paweł Łangowski <pawlan@st.amu.edu.pl>
2024-05-07 08:11:22 +0200

    9. Favorite Places App - create UI

---
+++ 9. Favorite Places App/favorite_places_app/lib/screens/places.dart
@@ -0,0 +1,42 @@
+import 'package:favorite_places_app/providers/user_places.dart';
+import 'package:favorite_places_app/screens/add_place.dart';
+import 'package:favorite_places_app/widgets/places_list.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class PlaceScreen extends ConsumerWidget {
+  const PlaceScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userPlaces = ref.watch(userPlacesProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Your Places"),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 30.0),
+            child: IconButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const AddPlaceScreen(),
+                  ),
+                );
+              },
+              icon: const Icon(
+                Icons.add,
+                color: Colors.white,
+                size: 24,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: PlacesList(
+        places: userPlaces,
+      ),
+    );
+  }
+}
